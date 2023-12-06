d48d59e2df3c92261e3b3b2c4b4ca43367e2ffd3
Rashid Gitinov <gitinovrashid@gmail.com>
2023-12-06 15:11:59 +0300

    hmhmhm

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.SQLOutput;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
+        Path pathOfFile = Paths.get("Z:\\Java\\My_Projects\\testEx\\resources\\app.properties.txt");
+        Map<String, String> conversionMap = importDataAndConversion(pathOfFile);
+        System.out.println(conversionMap.toString());
 
     }
     public static Map<String, String> importDataAndConversion(Path path){
