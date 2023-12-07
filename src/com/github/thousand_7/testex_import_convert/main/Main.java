3b5f20596b8efbab26b6aadde69de934337b75f9
Rashid Gitinov <gitinovrashid@gmail.com>
2023-12-07 14:46:17 +0300

    fix error compiler

--- src/com/github/thousand_7/testex_import_convert/main/Main.java
+++ src/com/github/thousand_7/testex_import_convert/main/Main.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Path pathOfFile = Paths.get("com\\github\\thousand_7\\testex_import_convert\\main\\resourses\\app.properties.txt");
+        Path pathOfFile = Paths.get("Z:\\Java\\My_Projects\\testEx\\src\\com\\github\\thousand_7\\testex_import_convert\\main\\resourses\\app.properties.txt");
         Converter converter = new Converter(pathOfFile);
         Map<String, String> conversionMap = converter.getMap();
         System.out.println(conversionMap.toString());
